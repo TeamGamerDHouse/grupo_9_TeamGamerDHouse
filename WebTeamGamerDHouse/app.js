f97c77481d9954057646185569d14eecc9bdd67a
DanielUsugaB <danusbet@gmail.com>
2022-07-04 20:06:03 -0500

    ejs y controllers

--- WebTeamGamerDHouse/app.js
+++ WebTeamGamerDHouse/app.js
@@ -4,43 +4,17 @@ const express = require("express");
 const app = express();
 const path =require("path");
 
-const publicPath = path.resolve(__dirname, 'public');
+const publicPath = path.resolve(__dirname, '../public');
 
+const mainRoutes = require('./routes/mainRoutes');
 
-// MIDDELWARE
 app.use(express.static(publicPath));
 
+app.set('view engine', 'ejs');
 
-// RUTEO
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
-})
-app.get('/login', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-    
-})
-/*app.get('/prueba', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './views/prueba.html'));
-})*/
-
-app.get('/carritocompra', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './views/carritocompra.html'));
-})
-app.get('/detalleproductos', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/detalleproductos.html'));
-})
-
-app.get('/register', (req, res)=>{
-
-    res.sendFile(path.resolve(__dirname,'./views/register.html'));
-
-})
-
-
-
+app.use('/', mainRoutes);
 
 // LEVANTO SERVIDOR
-
 app.listen(3000,()=>{
     console.log("Servidor corriendo en puerto 3000");
 })
