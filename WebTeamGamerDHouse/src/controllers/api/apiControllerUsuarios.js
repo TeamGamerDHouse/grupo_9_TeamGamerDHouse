be3b9a67c60aeed3094a32f1493c69fdf5be894b
Javier Garcia Rios <104571873+JavierGRios@users.noreply.github.com>
2022-09-28 10:24:35 -0300

    Creacion Apis usuarios - ver detalle

---
+++ WebTeamGamerDHouse/src/controllers/api/apiControllerUsuarios.js
@@ -0,0 +1,40 @@
+const path =require('path');
+const { validationResult }= require('express-validator');
+const bcryptjs =require('bcryptjs');
+const db= require('../../../database/models/index');
+const OP= db.sequelize.Op;
+
+const apiControllerUsuarios ={
+
+    listadeUsuarios :(req,res)=>{
+
+        db.Usuario.findAll()
+        .then(usuarios => {
+            return res.status(200).json({
+                total:usuarios.length,
+                data: usuarios,
+                status:200
+            })})
+        
+           
+    },
+
+    // usuario por ID (byPK)
+
+    detalleUsuario: (req,res)=>{
+        db.Usuario.findByPk(req.params.id)
+        .then(usuario => {
+            return res.status(200).json({
+                data:usuario,
+                status:200
+            })})
+        
+              
+            }
+
+    
+    }
+
+
+
+module.exports = apiControllerUsuarios
